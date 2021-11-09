687509659889394fa521a378e3ef403b519fc6fd
hyowon lee <woni03721@naver.com>
2021-11-09 22:33:53 +0900

    tab activity

---
+++ app/src/main/java/com/example/vocaproject/TabActivity.java
@@ -0,0 +1,44 @@
+package com.example.temp;
+
+import android.app.ActivityGroup;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class TabActivity extends ActivityGroup {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab);
+
+        TabHost tabHost = (TabHost) findViewById(R.id.host);
+        tabHost.setup(getLocalActivityManager());
+
+        TabHost.TabSpec spec = tabHost.newTabSpec("tab1")
+                .setIndicator(null,getResources().getDrawable(R.drawable.icon_wordbook))
+                .setContent(new Intent(this, WordbookActivity.class));
+        tabHost.addTab(spec);
+
+        // 테스트 액티비티 추가 예정
+        spec = tabHost.newTabSpec("tab2")
+                .setIndicator(null, getResources().getDrawable(R.drawable.icon_test))
+                .setContent(new Intent(this,Maintest.class));
+        tabHost.addTab(spec);
+
+        // 즐겨찾기 액티비티 추가 예정
+        spec = tabHost.newTabSpec("tab3")
+                .setIndicator(null, getResources().getDrawable(R.drawable.icon_heart))
+                .setContent(new Intent(this,Bookmark.class));
+        tabHost.addTab(spec);
+
+        // 탐색 액티비티 추가 예정
+        spec = tabHost.newTabSpec("tab4")
+                .setIndicator(null, getResources().getDrawable(R.drawable.icon_search))
+                .setContent(new Intent(this,SearchWord.class));
+        tabHost.addTab(spec);
+
+        tabHost.setCurrentTabByTag("tab1");
+    }
+
+}
