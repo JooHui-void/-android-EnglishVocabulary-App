7bf73371845e4b7ec4864b6e638cac8e7f59063f
jiwon <ljw010512@naver.com>
2021-11-16 12:35:22 +0900

    DB update

--- app/src/main/java/com/example/vocaproject/AppDatabase.java
+++ app/src/main/java/com/example/vocaproject/AppDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Word.class, WordBook.class}, version = 1, exportSchema = false)
+@Database(entities = {Word.class, WordBook.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     // 데이터에 접근하게 함 -> 데이터 삽입, 조희, 삭제 등
     public abstract WordDao wordDao();
