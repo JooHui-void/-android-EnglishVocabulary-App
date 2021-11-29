a76ddfa310153f1634d0c6658d2e22fa56719c79
hyowon lee <woni03721@naver.com>
2021-11-30 01:19:44 +0900

    디테일 수정

--- app/src/main/java/com/example/vocaproject/DailyWordbookActivity.java
+++ app/src/main/java/com/example/vocaproject/DailyWordbookActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class DailyWordbookActivity extends AppCompatActivity implements OnWordItemClick {
+public class DailyWordbookActivity extends AppCompatActivity
+        implements OnWordItemClick
+//                ,AdapterView.OnItemClickListener
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,4 +45,13 @@ public class DailyWordbookActivity extends AppCompatActivity implements OnWordIt
         WordDao WDao = db.wordDao();
         WDao.setUpdateWord(word);
     }
+
+//    @Override
+//    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+//
+//        Intent intent = new Intent(this, FlashCardTest.class);
+//        intent.putExtra("DAY",day);
+//
+//        startActivity(intent);
+//    }
 }
