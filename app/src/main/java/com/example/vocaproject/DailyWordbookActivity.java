f31ee0d77491a56a5b0f17b68e504627ebc04a69
hyowon lee <woni03721@naver.com>
2021-11-30 22:24:48 +0900

    테스트 수정

--- app/src/main/java/com/example/vocaproject/DailyWordbookActivity.java
+++ app/src/main/java/com/example/vocaproject/DailyWordbookActivity.java
@@ -21,7 +21,7 @@ public class DailyWordbookActivity extends AppCompatActivity
         implements OnWordItemClick
 //                ,AdapterView.OnItemClickListener
 {
-
+    int date = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,16 +41,15 @@ public class DailyWordbookActivity extends AppCompatActivity
         WordBookDao WBDao = db.wordBookDao();
 
         Intent intent = getIntent();
-        int date = intent.getIntExtra("DAY",0);
+        date = intent.getIntExtra("DAY",0);
 
-        List<Word> tmp_words = WDao.getDailyData(date);
-        ArrayList<Word> words = new ArrayList<>();
-        for(int i=0; i<tmp_words.size(); i++) words.add(tmp_words.get(i));
-        // words 정렬하기
+        List<Word> words = WDao.getDailyData(date);
 
         WordAdapter adapter = new WordAdapter(this, R.layout.custom_word_item, words,this);
         wordList.setAdapter(adapter);
     }
+
+
     @Override
     public void onWordItemClick(Word word){
         AppDatabase db = AppDatabase.getInstance(this);
@@ -58,22 +57,17 @@ public class DailyWordbookActivity extends AppCompatActivity
         WDao.setUpdateWord(word);
     }
 
-//    @Override
-//    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-//
-//        Intent intent = new Intent(this, FlashCardTest.class);
-//        intent.putExtra("DAY",day);
-//
-//        startActivity(intent);
-//    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         //여기서부터 작성
+        Intent intent;
         switch (item.getItemId()) {
             case R.id.play:
-
-
+                intent = new Intent(this, FlashCardTest.class);
+                intent.putExtra("VocaDay",date);
+                startActivity(intent);
             case R.id.menu:
+
 
             default:
                 return super.onOptionsItemSelected(item);
